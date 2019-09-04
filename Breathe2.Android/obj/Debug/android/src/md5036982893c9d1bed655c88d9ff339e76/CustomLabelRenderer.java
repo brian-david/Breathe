9ac7309dd0edf6449d41aea1b0d4aa87f427591f
Brian <brian.irons@icloud.com>
2019-09-04 06:42:24 +0200

    comm

---
+++ Breathe2.Android/obj/Debug/android/src/md5036982893c9d1bed655c88d9ff339e76/CustomLabelRenderer.java
@@ -0,0 +1,54 @@
+package md5036982893c9d1bed655c88d9ff339e76;
+
+
+public class CustomLabelRenderer
+	extends md51558244f76c53b6aeda52c8a337f2c37.LabelRenderer
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("breath.Droid.Renderers.CustomLabelRenderer, Breathe2.Android", CustomLabelRenderer.class, __md_methods);
+	}
+
+
+	public CustomLabelRenderer (android.content.Context p0, android.util.AttributeSet p1, int p2)
+	{
+		super (p0, p1, p2);
+		if (getClass () == CustomLabelRenderer.class)
+			mono.android.TypeManager.Activate ("breath.Droid.Renderers.CustomLabelRenderer, Breathe2.Android", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android:System.Int32, mscorlib", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public CustomLabelRenderer (android.content.Context p0, android.util.AttributeSet p1)
+	{
+		super (p0, p1);
+		if (getClass () == CustomLabelRenderer.class)
+			mono.android.TypeManager.Activate ("breath.Droid.Renderers.CustomLabelRenderer, Breathe2.Android", "Android.Content.Context, Mono.Android:Android.Util.IAttributeSet, Mono.Android", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public CustomLabelRenderer (android.content.Context p0)
+	{
+		super (p0);
+		if (getClass () == CustomLabelRenderer.class)
+			mono.android.TypeManager.Activate ("breath.Droid.Renderers.CustomLabelRenderer, Breathe2.Android", "Android.Content.Context, Mono.Android", this, new java.lang.Object[] { p0 });
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Breathe2.Android/obj/Debug/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 0db709664370c71295ee72b00d509e13e8c095a0 */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1512
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 396145
+	.include	"typemap.mj.inc"
